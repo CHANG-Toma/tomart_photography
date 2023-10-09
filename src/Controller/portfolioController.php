84e16a4af2155c00b56b663f0ce2d1d77a1c7163
CHANG-Toma <toma11chang@gmail.com>
2023-10-09 14:30:07 +0200

    amélioration html

--- src/Controller/portfolioController.php
+++ src/Controller/portfolioController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\ImagesModeleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,17 +18,20 @@ private $Images;
     }
 
     ///////////////Portfolio////////////////////
-    #[Route('/', name: 'portfolio')]
+    #[Route('/portfolio', name: 'portfolio')]
     public function portfolio(): Response
     {
+        $ImagesTravel = $this->Images->findBy("Travel");
+        $ImagesPortrait = $this->Images->findBy("Portrait");
+        $ImagesSport = $this->Images->findBy("Sport");
         $allImage = $this->Images->findAll();
-
         //dd($allImage);
 
         return $this->render('portfolio.html.twig', [
-            'allImage' => $allImage
+            'allImage' => $allImage,
+            'ImagesTravel' => $ImagesTravel,
+            'ImagesPortrait' => $ImagesPortrait,
+            'ImagesSport' => $ImagesSport
         ]);
     }
-
-
 }
